312dd7ba45863ea9b5fe12dd2a96fc47790d36b4
codebendr-product-designer <codebendr@gmail.com>
2021-10-14 21:38:40 +0000

    added executable target

---
+++ Koch/Sources/Koch/main.swift
@@ -0,0 +1,16 @@
+import Foundation
+import HTML
+
+struct MySite: Rule {
+    var body: some Rule {
+        Write(contents: html {
+            HTML.body {
+                h1 { "Hello, World!" }
+            }
+        }, to: "index.html")
+    }
+}
+
+MySite().execute()
+
+print("hi")
